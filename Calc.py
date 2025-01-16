2e87215e3fac128295311abf9c8b0d072cfbcd17
Manasyu Chaudhari <Manasyu.chaudhari@gmail.com>
2025-01-16 14:43:55 -0600

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,5 +1,7 @@
 input1 = int(input('Enter your first number '))
 input2 = int(input('Enter your second number '))
 
-print(f'The result of your multiplication is= {input1 * input2}')
+
+print(f'The result of multiplication is = {input1 * input2}')
+print(f'The result of substraction is = {input1 - input2}')
 
