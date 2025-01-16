161aa788bcf2e265ce533b69b76fb6c4af7beb5d
Manasyu Chaudhari <Manasyu.chaudhari@gmail.com>
2025-01-16 14:53:21 -0600

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,5 +1,12 @@
+import random
+
+
 input1 = int(input('Enter your first number '))
 input2 = int(input('Enter your second number '))
+rand_num = random.randint(0,10)
+
+
+
 
 
 print(f'The result of multiplication is = {input1 * input2}')
