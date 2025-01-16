da052a3287e71b7ece1e6f63a5064c189f2fa089
Manasyu Chaudhari <Manasyu.chaudhari@gmail.com>
2025-01-16 14:57:33 -0600

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,7 +1,11 @@
 input1 = int(input('Enter your first number '))
 input2 = int(input('Enter your second number '))
+input3 = int(input('Enter your third number '))
 
 
 print(f'The result of multiplication is = {input1 * input2}')
-print(f'The result of substraction is = {input1 - input2}')
+print(f'Difference of num1 & num2 = {input1 - input2}')
+print(f'Differnece of num2 & num3 = {input2 - input3}')
+
+
 
