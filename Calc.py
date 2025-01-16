eeb5733b7408016d934e617aa70c75bb245ceff3
Manasyu Chaudhari <Manasyu.chaudhari@gmail.com>
2025-01-16 15:42:55 -0600

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -5,10 +5,6 @@ input1 = int(input('Enter your first number '))
 input2 = int(input('Enter your second number '))
 rand_num = random.randint(0,10)
 
-
-
-
-
 print(f'The result of multiplication is = {input1 * input2}')
 print(f'The result of substraction is = {input1 - input2}')
 
